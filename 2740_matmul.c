eddf04d228718e23ad29de6a7cc03ececab750a8
jina <jina040602inu.ac.kr>
2026-03-03 21:27:10 +0900

    2740_ans

--- 2740_matmul.c
+++ 2740_matmul.c
@@ -4,8 +4,8 @@ int main(void){
     scanf("%d %d", &n, &m);
 
     int A[100][100];
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
             scanf("%d", &A[i][j]);
         }
     }
@@ -13,17 +13,17 @@ int main(void){
     int k;
     scanf("%d %d", &m, &k);
     int B[100][100];
-    for(int i=0;i<k;i++){
-        for(int j=0;j<m;j++){
+    for(int i=0;i<m;i++){
+        for(int j=0;j<k;j++){
             scanf("%d", &B[i][j]);
         }
     }
 
     int C[100][100]={0, };
     for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            for(int h=0;h<k;h++){
-                C[i][h]+=A[i][j]*B[j][h];
+        for(int j=0;j<k;j++){
+            for(int h=0;h<m;h++){
+                C[i][j]+=A[i][h]*B[h][j];
             }
         }
     }
