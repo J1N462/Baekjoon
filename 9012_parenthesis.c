9fd4b850d4251bf973374bd35aa39f652af77343
jina <jina040602inu.ac.kr>
2026-03-14 16:51:50 +0900

    9012_ans

--- 9012_parenthesis.c
+++ 9012_parenthesis.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int stack[51];
-int top=-1;
+int top;
+void init(){
+    top=-1;
+}
 int is_empty(){
     return (top==-1);
 }
-void push(char *a){
+void push(char a){
     stack[++top]=a;
 }
 int pop(){
@@ -14,23 +17,36 @@ int main(void){
     int T;
     scanf("%d", &T);
     for(int i=0;i<T;i++){
-        char str[51]={'\0'}; //초기화되나 아니면 초기화 함수 만들어야하나?
+        int print=0; //Yes
+        init();
+        char str[51];
         scanf("%s", str);
-        for(int i=0;str[i]!='\0';i++){
-            if(str[i]=='('){
+        for(int j=0;str[j]!='\0';j++){
+            if(str[j]=='('){
                 push('(');
             }
             else{
                 if(is_empty()){
-                    printf("NO");
-                    continue;
+                    print=1; //NO
+                    break;
                 }
                 pop();
             }
         }
-        if(is_empty()){
-            printf("YES");
+        if(is_empty()&&print==0){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+
         }
     }
     return 0;
 }
+
+/*
+괄호가 남아잇는 채로 반복문이 종료된 경우
+괄호가 없는데 오른쪽괄호가 들어온 경우
+-no
+else -yes
+*/
